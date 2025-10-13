cdea4bdf3160d77325f2a19c344d6b5aebdacdb0
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2025-10-13 10:59:23 +0530

    first commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser()); // <-- added
 
 // CORS config for frontend
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:[ 'http://localhost:5173','https://college-resource-sharing.vercel.app'],
   credentials: true // <-- allow cookies
 }));
 
